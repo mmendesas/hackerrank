24e049d4a180c99df2046d44a6f2f3e426a04d21
Marcio Mendes <17890831+mmendesas@users.noreply.github.com>
2022-03-08 00:23:26 +0100

    add comments to median_two_sorted

--- src/leetcode/04_median_two_sorted_arrays.js
+++ src/leetcode/04_median_two_sorted_arrays.js
@@ -2,45 +2,56 @@
 // https://leetcode.com/problems/median-of-two-sorted-arrays/
 
 const merge = (nums1, nums2) => {
-  let last = nums1.length + nums2.length - 1
-  const arr = Array(nums1.length + nums2.length).fill()
   let m = nums1.length
   let n = nums2.length
 
-  while(m > 0 && n > 0){
-     if (nums1[m - 1] > nums2[n - 1]) {
-        arr[last] = nums1[m - 1]
-        m--;
-      } else {
-        arr[last] = nums2[n - 1]
-        n--;
-      }
-      last--;
+  // 1 - create blank arr with full size (m+n)
+  let last = (m + n) - 1
+  const arr = Array(m + n).fill()
+
+  // 2 - compare 2 last items from each list
+  //   - use max and move cursor
+  while (m > 0 && n > 0) {
+    if (nums1[m - 1] > nums2[n - 1]) {
+      arr[last] = nums1[m - 1]
+      m--;
+    } else {
+      arr[last] = nums2[n - 1]
+      n--;
+    }
+    last--;
   }
 
-  // fill nums1 with leftover from nums2
+  // 3 - fill arr with leftover from nums2
   while (n > 0) {
-    arr[last] = nums2[n-1]
+    arr[last] = nums2[n - 1]
     n--;
     last--;
   }
 
-  // fill nums2 with leftover from nums1
+  // 4 - fill arr with leftover from nums1
   while (m > 0) {
-    arr[last] = nums1[m-1]
+    arr[last] = nums1[m - 1]
     m--;
     last--;
   }
+
   return arr
 }
 
 const findMedianSortedArrays = (nums1, nums2) => {
+  // 1 - merge sorted arr
+  const arr = merge(nums1, nums2)
+
+  // 2 - get middle/isEven
   const size = nums1.length + nums2.length
   const middle = Math.floor(size / 2)
-  const arr = merge(nums1, nums2)
   const isEven = size % 2 == 0;
 
-  const res = isEven ? (arr[middle-1] + arr[middle])/2 : arr[middle]
+  // 3 - get average based on odd/even
+  const res = isEven ? (arr[middle - 1] + arr[middle]) / 2 : arr[middle]
+
+  // 4 - return format
   return res == 0 ? 0 : res.toFixed(5)
 }
 
