0bbb6b47cbf25ae9efd1c900134b5b924a376053
Marcio Mendes <17890831+mmendesas@users.noreply.github.com>
2023-08-22 13:41:30 -0300

    feat: new year chaos

--- src/hackerrank/interview_prep_kit/1_week_prep/010_new_year_chaos.js
+++ src/hackerrank/interview_prep_kit/1_week_prep/010_new_year_chaos.js
@@ -1,20 +1,44 @@
-function minimumBribes(arr) {
-
-  let chaotic = false;
-  let bribes = 0;
-
-  for (let i = 0; i < arr.length; i++) {
-    if (arr[i] - (i + 1) > 2) {
-      chaotic = true;
-      break;
-    }
-
-    for (let j = Math.max(0, arr[i] - 2); j < i; j++) {
-      if (arr[j] > arr[i]) bribes += 1;
-    }
+function rotateItem(arr, pos, dist) {
+  const list = [...arr]
+  for (let i = 0; i < dist; i++) {
+    let item = list[pos + i];
+    list[pos + i] = list[pos + i + 1]
+    list[pos + i + 1] = item
   }
-
-  return chaotic ? 'Too chaotic' : bribes
+  return list;
 }
 
-console.log(minimumBribes([1, 2, 3, 5, 4, 6, 7, 8]))
+// loop in reverse order and use size as the number at position x
+// list with 3 items, last pos is the number 3 (size)
+// then calculate the move required to order it.
+
+function minimumBribes(arr) {
+  let currentNumber = arr.length;
+  let bribes = 0;
+
+  for (let i = currentNumber - 1; i >= 0; i--) {
+    if (arr[i] === currentNumber) {
+
+      let expectedPos = currentNumber - 1;
+      let distance = expectedPos - i;
+
+      if (distance > 2) {
+        bribes = 'Too chaotic'
+        break;
+      }
+
+      if (distance > 0) {
+        arr = rotateItem(arr, i, distance)
+        bribes += distance
+      }
+
+      currentNumber--;
+      i = currentNumber;
+    }
+  }
+  console.log(bribes)
+}
+
+minimumBribes([2, 1, 5, 3, 4]) // 3
+minimumBribes([2, 5, 1, 3, 4]) // Too chaotic
+minimumBribes([1, 2, 3, 5, 4, 6, 7, 8]) // 1
