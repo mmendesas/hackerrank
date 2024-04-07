37db7a027fb59e36f7e6a767f1e843980d9804b6
Marcio Mendes <17890831+mmendesas@users.noreply.github.com>
2024-04-07 00:38:43 -0300

    add more patterns

--- src/forward/patterns.js
+++ src/forward/patterns.js
@@ -147,27 +147,128 @@ const pattern10 = (n = 4) => {
   }
 };
 
-const fnByName = {
-  pattern01: pattern01,
-  pattern02: pattern02,
-  pattern03: pattern03,
-  pattern04: pattern04,
-  pattern05: pattern05,
-  pattern06: pattern06,
-  pattern07: pattern07,
-  pattern08: pattern08,
-  pattern09: pattern09,
-  pattern10: pattern10,
+const pattern11 = (n = 4) => {
+  for (let i = 1; i <= n; i++) {
+    for (let j = 1; j <= i; j++) {
+      const num = (i + j) % 2 === 0 ? 1 : 0;
+      process.stdout.write(` ${num}`);
+    }
+    process.stdout.write("\n");
+  }
 };
 
-Array.from({
-  length: Object.keys(fnByName).length,
-}).forEach((_, index) => {
-  const num = `${index + 1}`.padStart(2, "0");
-  const fnName = `pattern${num}`;
+const pattern12 = (n = 4) => {
+  const max = 2 * n;
 
-  console.log("_".repeat(50), `\n\n${fnName}\n`);
-  if (typeof fnByName[fnName]) {
-    fnByName[fnName](5);
+  for (let i = 1; i <= n; i++) {
+    const spaces = max - i * 2;
+
+    let line = "";
+    for (let j = 1; j <= max; j++) {
+      if (j <= i) line += ` ${j}`;
+    }
+
+    const reverse = [...line].reverse().join("");
+    const lineFinal = `${line}${"  ".repeat(spaces + 1)}${reverse}`;
+
+    process.stdout.write(lineFinal);
+    process.stdout.write("\n");
   }
-});
+};
+
+const pattern13 = (n = 6) => {
+  let count = 1;
+  for (let i = 1; i <= n; i++) {
+    for (let j = 1; j <= i; j++) {
+      const toPrint = String(count).padEnd(2, " "); // alignment
+      process.stdout.write(` ${toPrint}`);
+      count++;
+    }
+    process.stdout.write("\n");
+  }
+};
+
+const pattern14 = (n = 4) => {
+  // Using for loop for (A-Z):
+  let alphabet = "";
+  for (i = 65; i <= 90; i++) {
+    alphabet += String.fromCharCode(i);
+  }
+
+  for (let i = 0; i < n; i++) {
+    for (let j = 0; j <= i; j++) {
+      process.stdout.write(" " + alphabet[j % alphabet.length]);
+    }
+    process.stdout.write("\n");
+  }
+};
+
+const pattern15 = (n = 3) => {
+  // Using for loop for (A-Z):
+  let alphabet = "";
+  for (i = 65; i <= 90; i++) {
+    alphabet += String.fromCharCode(i);
+  }
+
+  for (let i = 0; i < n; i++) {
+    for (let j = 0; j < n - i; j++) {
+      process.stdout.write(" " + alphabet[j % alphabet.length]);
+    }
+    process.stdout.write("\n");
+  }
+};
+
+const pattern16 = (n = 5) => {
+  // Using for loop for (A-Z):
+  let alphabet = "";
+  for (i = 65; i <= 90; i++) {
+    alphabet += String.fromCharCode(i);
+  }
+
+  for (let i = 0; i < n; i++) {
+    for (let j = 0; j <= i; j++) {
+      process.stdout.write(" " + alphabet[i]);
+    }
+    process.stdout.write("\n");
+  }
+};
+
+// RESULTS
+
+const printAll = () => {
+  const fnByName = {
+    pattern01: pattern01,
+    pattern02: pattern02,
+    pattern03: pattern03,
+    pattern04: pattern04,
+    pattern05: pattern05,
+    pattern06: pattern06,
+    pattern07: pattern07,
+    pattern08: pattern08,
+    pattern09: pattern09,
+    pattern10: pattern10,
+    pattern11: pattern11,
+    pattern12: pattern12,
+    pattern13: pattern13,
+    pattern14: pattern14,
+    pattern15: pattern15,
+    pattern16: pattern16,
+  };
+
+  Array.from({
+    length: Object.keys(fnByName).length,
+  }).forEach((_, index) => {
+    const num = `${index + 1}`.padStart(2, "0");
+    const fnName = `pattern${num}`;
+
+    console.log("_".repeat(50), `\n\n${fnName}\n`);
+    if (typeof fnByName[fnName]) {
+      fnByName[fnName](5);
+    }
+  });
+};
+
+// unique test
+// pattern16();
+
+printAll();
