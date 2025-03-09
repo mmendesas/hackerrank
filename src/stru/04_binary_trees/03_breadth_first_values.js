ed83e20d8cabdab2c06371a5bd9add497d4acbe7
Marcio Mendes <17890831+mmendesas@users.noreply.github.com>
2025-03-09 13:17:35 -0300

    breadth first algo

---
+++ src/stru/04_binary_trees/03_breadth_first_values.js
@@ -0,0 +1,46 @@
+class Node {
+  constructor(val) {
+    this.val = val;
+    this.left = null;
+    this.right = null;
+  }
+}
+
+const a = new Node("a");
+const b = new Node("b");
+const c = new Node("c");
+const d = new Node("d");
+const e = new Node("e");
+const f = new Node("f");
+
+a.left = b;
+a.right = c;
+
+b.left = d;
+b.right = e;
+c.right = f;
+
+//     a
+//    / \
+//   b   c
+//  / \   \
+// d   e   f
+
+// iterative approach
+const breadthFirstValues = (root) => {
+  if (!root) return [];
+
+  const values = [];
+  const queue = [root];
+
+  while (queue.length > 0) {
+    const current = queue.shift();
+    values.push(current.val);
+
+    if (current.left) queue.push(current.left);
+    if (current.right) queue.push(current.right);
+  }
+  return values;
+};
+
+console.log(breadthFirstValues(a));
